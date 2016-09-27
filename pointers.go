7cdaf4bf0e8971b4b7be74b82da68c385c14d454
Eduard Semsch <eduard.semsch@seznam.cz>
2016-09-27 21:26:20 +0000

    Learning pointers

---
+++ pointers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	vals := []string{"a", "b", "c", "d", "e"}
+	other_vals := []string{"x", "y", "z"}
+
+	fmt.Println(vals)
+	fmt.Println(other_vals)
+
+	swap(&vals, &other_vals)
+
+	fmt.Println(vals)
+	fmt.Println(other_vals)
+
+	var x *[]string = &vals
+	var y *[]string = &other_vals
+
+	swap(x, y)
+	fmt.Println(*x)
+	fmt.Println(*y)
+
+	swap(x, y)
+	fmt.Println(*x)
+	fmt.Println(*y)
+
+}
+
+func swap(a *[]string, b *[]string) {
+	x := *a
+	println(x)
+	println(a)
+	println(*a)
+	*a = *b
+	println(a)
+	println(*a)
+
+	*b = x
+}
