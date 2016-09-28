b6e66adc2536987ebad1b0c7e4417e107ad3bcdc
Eduard Semsch <eduard.semsch@seznam.cz>
2016-09-28 15:46:28 +0000

    wordcount: File reading finished

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"io"
+	"io"
 	"os"
 )
 
@@ -15,17 +15,33 @@ func main() {
 
 	fileName := args[1]
 
-	file,err := os.Open(fileName)
-	if err!=nil {
+	file, err := os.Open(fileName)
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "Couldn't open file %s", fileName)
 		os.Exit(1)
 	}
 
 	wholeFile := []byte{}
-	buffer := make(byte[],4096)
+	buffer := make([]byte, 4096)
 
 	for {
+		n, err := file.Read(buffer)
+		if n == 0 && err == io.EOF {
+			err := file.Close()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Couldn't close file %s", fileName)
+				os.Exit(1)
+			}
+			break
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading from file %s. Error: ", fileName, err)
+			os.Exit(1)
+		} else {
+			wholeFile = append(wholeFile, buffer[0:n]...)
+		}
 
 	}
+
+	fmt.Println(string(wholeFile))
 
 }
