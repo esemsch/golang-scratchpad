0f8da0db97702a5fe0f32ddd0bc665b4ce90a930
Eduard Semsch <eduard.semsch@seznam.cz>
2016-09-30 14:08:42 +0000

    json_yml: doing some experiments with yaml, for some reason there doesn't seem to be a yaml equivalent to json generic parsing from the go packages

--- json_yml/yml.go
+++ json_yml/yml.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
 )
 
 func main() {
+	content, err := ioutil.ReadFile(`simple.yml`)
+	if err != nil {
+		panic(err)
+	}
 
+	var x struct {
+		Very string `yaml:"very"`
+	}
+
+	err = yaml.Unmarshal(content, &x)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(x)
+
+	mrsh, err := yaml.Marshal(x)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(mrsh))
+
+	var mp = map[string]interface{}{
+		"something": 10,
+		"else": map[string][]string{
+			"cool": []string{"stuff", "stuff", "stuff"},
+		},
+	}
+	fmt.Println(mp)
+
+	mrsh, err = yaml.Marshal(mp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(mrsh))
 }
